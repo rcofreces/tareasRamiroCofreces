353cc3ab1dc4d6b778959364ae697f040548c4c8
Ramiro Cofreces <cofrecesramiro@gmail.com>
2022-08-06 11:27:14 -0300

    06/08/22

--- APP CONTADOR/index.js
+++ APP CONTADOR/index.js
@@ -1,24 +1,30 @@
+const botonRestar = document.getElementById("button1");
+const botonSumar = document.getElementById("button2");
+const botonSumarDeA = document.getElementById("button3");
+const myInput = document.querySelector("input");
+const mySpan = document.getElementById("resultado");
+
 let cantidad = 0;
 
 function decrementar() {
     cantidad--;
+    mySpan.innerHTML = cantidad;
     console.log("Decrementaste" + cantidad);
 }
 
 function incrementar() {
     cantidad++;
+    mySpan.innerHTML = cantidad;
     console.log("Incrementaste" + cantidad);
 }
 
-let botonRestar = document.getElementById("button1");
 botonRestar.addEventListener("click", decrementar);
-
-let botonSumar = document.getElementById("button2");
 botonSumar.addEventListener("click", incrementar);
 
+
+
 /*function incrementarDeA() {
+    mySpan = myInput.value
     cantidad++;
     console.log("Incrementaste" + cantidad);
 }*/
-
-//<input type="text" class="resultado" id="resultadoTotal">
