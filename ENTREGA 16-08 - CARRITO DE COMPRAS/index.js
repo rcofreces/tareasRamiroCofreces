5bae626688d2841e96d0542a1f3e14bbb79ad49b
Ramiro Cofreces <cofrecesrbles@tdf.edu.ar>
2022-08-09 16:00:14 -0300

    09/08/22

--- ENTREGA 16-08 - CARRITO DE COMPRAS/index.js
+++ ENTREGA 16-08 - CARRITO DE COMPRAS/index.js
@@ -1,113 +1,63 @@
-const botonAgregar = document.getElementById("button1");
-const botonEliminarUltimo = document.getElementById("button2");
-const botonEliminarTodos = document.getElementById("button3");
-const botonCalcularCompra = document.getElementById("button4");
-const myInputProducto = document.getElementById("producto");
-const myInputPrecio = document.getElementById("precio");
-const myInputCantidad = document.getElementById("cantidad");
-const myList = document.getElementById("lista");
-const mySpan = document.getElementById("total");
+let miProducto = document.querySelector(".myInputProducto");
+let miCantidad = document.querySelector(".myInputCantidad");
+let miPrecio = document.querySelector(".myInputPrecio");
+let miOrden = document.querySelector(".myInputOrden");
+let miLista = document.querySelector(".myList");
+let miSpan = document.querySelector(".compraTotal");
 
-let contador = 0;
+let productos = [];
+let preciosUnitarios= [];
+let cantidadProductos = [];
+let total = 0;
 
-function agregar() {
-    let productoIngresado = myInputProducto.value;
-    let precioIngresado = myInputPrecio.value;
-    let cantidadIngresada = myInputCantidad.value;
-    let total = myInputPrecio.value * myInputCantidad.value;
-    myList.innerHTML = "PRODUCTO: " + `${productoIngresado}` +  " - CANTIDAD: " + `${cantidadIngresada}` + " - PRECIO UNITARIO: $" + `${precioIngresado}` + " - PRECIO TOTAL: $" + `${total}`;
-}
-
-function eliminarUltimo() {
-
-}
-
-function eliminarTodos() {
-    contador = 0;
-    myList.innerHTML = "";
-    mySpan.innerHTML = "";
-}
-
-function calcularCompra() {
-    contador = 0;
-    contador += parseInt(myInputPrecio.value) * parseInt(myInputCantidad.value);
-    mySpan.innerHTML = contador;
-}
-
-botonAgregar.addEventListener("click", agregar);
-botonEliminarUltimo.addEventListener("click", eliminarUltimo);
-botonEliminarTodos.addEventListener("click", eliminarTodos);
-botonCalcularCompra.addEventListener("click", calcularCompra);
-
-
-
-
-/*
-
-let miEntrada = document.querySelector(".myInput");
-let miLista = document.querySelector(".myOl");
-let miIndice = document.querySelector(".myInputOrden");
-
-let agregarTarea = () => {
+let agregarAlCarrito = () => {
     let miItem = document.createElement("li");
-    miItem.innerHTML = miEntrada.value;
+    miItem.innerHTML = miCantidad.value + " - " + miProducto.value + " - $" + miPrecio.value;
     miLista.appendChild(miItem);
-    miEntrada.value = "";
+    miProducto.value = "";
+    miCantidad.value = "";
+    miPrecio.value = "";
 }
 
-let eliminarUltTarea = () => {
+let eliminarUltimo = () => {
     miLista.removeChild(miLista.lastChild);
 }
 
-let eliminarTodo = () => {
+let vaciarCarrito = () => {
     miLista.innerHTML = "";
 }
 
-let eliminarOrdenTarea = () => {
-    let valor = miIndice.value;
-    miLista.removeChild(miLista.children[valor - 1]);
+//let calcularCompra = () => {
+//}
+
+let eliminarPorOrden = () => {
+    let orden = miOrden.value;
+    miLista.removeChild(miLista.children[orden - 1]);
+    miOrden.value = "";
 }
 
-let botonAgregar = document.querySelector(".addBtn");
-botonAgregar.addEventListener("click", agregarTarea);
-
-let botonEliminarUlt = document.querySelector(".removeBtn");
-botonEliminarUlt.addEventListener("click", eliminarUltTarea);
-
-let botonEliminarTodo = document.querySelector(".removeAll");
-botonEliminarTodo.addEventListener("click", eliminarTodo);
-
-let botonEliminarTarea = document.querySelector(".removeOrdenBtn");
-botonEliminarTarea.addEventListener("click", eliminarOrdenTarea);
-
-*/
+let botonAgregar = document.querySelector(".agregarProducto");
+botonAgregar.addEventListener("click", agregarAlCarrito);
+let botonEliminarUltimo = document.querySelector(".eliminarUltimo");
+botonEliminarUltimo.addEventListener("click", eliminarUltimo);
+let botonVaciar = document.querySelector(".vaciarCarrito");
+botonVaciar.addEventListener("click", vaciarCarrito);
+//let botonCalcularCompra = document.querySelector(".calcularCompra");
+//botonCalcularCompra.addEventListener("click", calcularCompra);
+let botonEliminarOrden = document.querySelector(".eliminarOrden");
+botonEliminarOrden.addEventListener("click", eliminarPorOrden);
 
 
 
 
 
 
-/*
+/* EJERCICIO DANI --------------------------------------------------------------------------------------------------------------
 
 let productos = [];
 let preciosUnitarios= [];
 let cantidadDeProductos = [];
 let total = 0;
-
-let miLista = document.querySelector(".carrito");
-
-let btnAgregar = document.getElementById("agregarProducto");
-btnAgregar.addEventListener("click",agregarProductoAlCarrito);
-
-let btnCalcular = document.getElementById("calcularTotal");
-btnCalcular.addEventListener("click", calcularCompra);
-
-let btnEliminarUltimo = document.getElementById("eliminarUltimo");
-btnEliminarUltimo.addEventListener("click", eliminarUltimo)
-
-
-let btnVaciarCarrito = document.getElementById("vaciarCarrito");
-btnVaciarCarrito.addEventListener("click", eliminarTodo);
 
 function agregarProductoAlCarrito(){
 
@@ -123,10 +73,10 @@ function agregarProductoAlCarrito(){
     console.log (cantidadDeProductos);
 
     let miItem = document.createElement("li");
-    miItem.innerHTML = "PRODUCTO | CANTIDAD | PRECIO_UNITARIO"
-    miItem.innerHTML = producto + "  |  " + cantidadDeProducto + "  |  " + precioUnitario; 
+    miItem.innerHTML = producto + "  |  " + cantidadDeProducto + "  |  " + precioUnitario;
 
-    miItem.classList.add("colorVerde");
+
+    //miItem.classList.add("colorVerde");
 
     miLista.appendChild(miItem);
 
@@ -151,20 +101,32 @@ function eliminarUltimo (){
     preciosUnitarios.pop();
     cantidadDeProductos.pop();
 
-    console.log (productos);
-    console.log (preciosUnitarios);
-    console.log (cantidadDeProductos);
+    miLista.removeChild(miLista.lastChild)
 }
 
 function eliminarTodo (){
     productos = [];
     preciosUnitarios= [];
     cantidadDeProductos = [];
+
+    miLista.innerHTML = "";
     
-    console.log (productos);
-    console.log (preciosUnitarios);
-    console.log (cantidadDeProductos);
 }
 
+
+
+let miLista = document.querySelector(".carrito");
+
+let btnAgregar = document.getElementById("agregarProducto");
+btnAgregar.addEventListener("click",agregarProductoAlCarrito);
+
+let btnCalcular = document.getElementById("calcularTotal");
+btnCalcular.addEventListener("click", calcularCompra);
+
+let btnEliminarUltimo = document.getElementById("eliminarUltimo");
+btnEliminarUltimo.addEventListener("click", eliminarUltimo)
+
+let btnVaciarCarrito = document.getElementById("vaciarCarrito");
+btnVaciarCarrito.addEventListener("click", eliminarTodo);
 
 */
