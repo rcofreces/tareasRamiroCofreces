7b947c8781a0a35f20cc9dd8b0b086791c44a166
Ramiro Cofreces <cofrecesramiro@gmail.com>
2022-08-09 19:57:41 -0300

    09/08/2022

--- ENTREGA 16-08 - CARRITO DE COMPRAS/index.js
+++ ENTREGA 16-08 - CARRITO DE COMPRAS/index.js
@@ -5,14 +5,17 @@ let miOrden = document.querySelector(".myInputOrden");
 let miLista = document.querySelector(".myList");
 let miSpan = document.querySelector(".compraTotal");
 
-let productos = [];
-let preciosUnitarios= [];
-let cantidadProductos = [];
-let total = 0;
+let arrayProductos = [];
+let arrayPrecios = [];
+let arrayCantidadProductos = [];
 
 let agregarAlCarrito = () => {
+    console.log("ingresa")
+    arrayProductos.push(miProducto);
+    arrayPrecios.push(miPrecio);
+    arrayCantidadProductos.push(miCantidad);
     let miItem = document.createElement("li");
-    miItem.innerHTML = miCantidad.value + " - " + miProducto.value + " - $" + miPrecio.value;
+    miItem.innerHTML = "Cantidad: " + miCantidad.value + " | Producto: " + miProducto.value + " | Precio: $" + miPrecio.value;
     miLista.appendChild(miItem);
     miProducto.value = "";
     miCantidad.value = "";
@@ -20,15 +23,26 @@ let agregarAlCarrito = () => {
 }
 
 let eliminarUltimo = () => {
+    arrayProductos.pop();
+    arrayPrecios.pop();
+    arrayCantidadProductos.pop();
     miLista.removeChild(miLista.lastChild);
 }
 
 let vaciarCarrito = () => {
+    arrayProductos = [];
+    arrayPrecios= [];
+    arrayCantidadProductos = [];
     miLista.innerHTML = "";
 }
 
-//let calcularCompra = () => {
-//}
+let calcularCompra = () => {
+    let total = 0;
+    for (let i = 0; i < arrayProductos.length; i++) {
+        total = total + (arrayPrecios[i] * arrayCantidadProductos[i]);
+    }
+    miSpan.innerHTML = total;
+}
 
 let eliminarPorOrden = () => {
     let orden = miOrden.value;
@@ -42,91 +56,7 @@ let botonEliminarUltimo = document.querySelector(".eliminarUltimo");
 botonEliminarUltimo.addEventListener("click", eliminarUltimo);
 let botonVaciar = document.querySelector(".vaciarCarrito");
 botonVaciar.addEventListener("click", vaciarCarrito);
-//let botonCalcularCompra = document.querySelector(".calcularCompra");
-//botonCalcularCompra.addEventListener("click", calcularCompra);
+let botonCalcularCompra = document.querySelector(".calcularCompra");
+botonCalcularCompra.addEventListener("click", calcularCompra);
 let botonEliminarOrden = document.querySelector(".eliminarOrden");
 botonEliminarOrden.addEventListener("click", eliminarPorOrden);
-
-
-
-
-
-
-/* EJERCICIO DANI --------------------------------------------------------------------------------------------------------------
-
-let productos = [];
-let preciosUnitarios= [];
-let cantidadDeProductos = [];
-let total = 0;
-
-function agregarProductoAlCarrito(){
-
-    let producto = document.getElementById("productoNuevo").value;
-    productos.push(producto);
-    let precioUnitario = document.getElementById("precioUnitario").value;
-    preciosUnitarios.push(precioUnitario);
-    let cantidadDeProducto = document.getElementById("cantidadUnitaria").value;
-    cantidadDeProductos.push(cantidadDeProducto);
-
-    console.log (productos);
-    console.log (preciosUnitarios);
-    console.log (cantidadDeProductos);
-
-    let miItem = document.createElement("li");
-    miItem.innerHTML = producto + "  |  " + cantidadDeProducto + "  |  " + precioUnitario;
-
-
-    //miItem.classList.add("colorVerde");
-
-    miLista.appendChild(miItem);
-
-    document.getElementById("productoNuevo").value = "";
-    document.getElementById("precioUnitario").value = "";
-    document.getElementById("cantidadUnitaria").value = "";
-
-}
-
-function calcularCompra(){
-    for(let i=0; i<productos.length;i++){
-        total = total + parseInt((preciosUnitarios[i] * cantidadDeProductos[i]));
-    }
-
-    let h2 = document.createElement("h2");
-    h2.innerHTML = "Total de su Compra " + total;
-    miLista.appendChild(h2);
-}
-
-function eliminarUltimo (){
-    productos.pop();
-    preciosUnitarios.pop();
-    cantidadDeProductos.pop();
-
-    miLista.removeChild(miLista.lastChild)
-}
-
-function eliminarTodo (){
-    productos = [];
-    preciosUnitarios= [];
-    cantidadDeProductos = [];
-
-    miLista.innerHTML = "";
-    
-}
-
-
-
-let miLista = document.querySelector(".carrito");
-
-let btnAgregar = document.getElementById("agregarProducto");
-btnAgregar.addEventListener("click",agregarProductoAlCarrito);
-
-let btnCalcular = document.getElementById("calcularTotal");
-btnCalcular.addEventListener("click", calcularCompra);
-
-let btnEliminarUltimo = document.getElementById("eliminarUltimo");
-btnEliminarUltimo.addEventListener("click", eliminarUltimo)
-
-let btnVaciarCarrito = document.getElementById("vaciarCarrito");
-btnVaciarCarrito.addEventListener("click", eliminarTodo);
-
-*/
